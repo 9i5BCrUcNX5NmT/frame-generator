57e17d2d469c16f4cbe8964c56150634c4412b79
9i5BCrUcNX5NmT <machinima40@yandex.ru>
2024-12-05 01:11:56 +0300

    Initial commit
    Initial commit 2
    
    Initial commit 3
    
    1

---
+++ keyboard parser/1.py
@@ -0,0 +1,66 @@
+import pandas as pd
+from pynput import keyboard
+import os
+import datetime
+import threading
+import time
+
+
+class KeyLogger:
+    def __init__(self, interval=10):
+        self.data = []
+        self.lock = threading.Lock()
+        self.interval = interval
+        self.writer_thread = threading.Thread(target=self.write_data_periodically)
+        self.writer_thread.daemon = True
+
+    def on_press(self, key):
+        try:
+            self.data.append(
+                {
+                    "key": key.char,
+                    "time": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                }
+            )
+        except AttributeError:
+            self.data.append(
+                {
+                    "key": str(key),
+                    "time": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                }
+            )
+
+    def on_release(self, key):
+        if key == keyboard.Key.esc:
+            # Stop listener
+            return False
+
+    def write_data_periodically(self):
+        while True:
+            with self.lock:
+                if self.data:
+                    df = pd.DataFrame(self.data)
+                    if os.path.exists("key_log.parquet"):
+                        existing_df = pd.read_parquet("key_log.parquet")
+                        combined_df = pd.concat([existing_df, df])
+                        combined_df.to_parquet(
+                            "key_log.parquet", compression="snappy", index=False
+                        )
+                    else:
+                        df.to_parquet(
+                            "key_log.parquet", compression="snappy", index=False
+                        )
+                    self.data = []
+            time.sleep(self.interval)
+
+    def start(self):
+        self.writer_thread.start()
+        with keyboard.Listener(
+            on_press=self.on_press, on_release=self.on_release
+        ) as listener:
+            listener.join()
+
+
+if __name__ == "__main__":
+    key_logger = KeyLogger(interval=10)  # запись данных в файл каждые 10 секунд
+    key_logger.start()
---
+++ model/src/model.rs
@@ -0,0 +1,113 @@
+use burn::{
+    nn::{
+        conv::{Conv2d, Conv2dConfig},
+        pool::{AdaptiveAvgPool2d, AdaptiveAvgPool2dConfig},
+        Dropout, DropoutConfig, Linear, LinearConfig, Relu,
+    },
+    prelude::*,
+};
+
+#[derive(Module, Debug)]
+pub struct Model<B: Backend> {
+    conv11: Conv2d<B>,
+    activation11: Relu,
+
+    // conv12: Conv2d<B>,
+    // activation12: Relu,
+
+    // conv13: Conv2d<B>,
+    // activation13: Relu,
+    conv21: Conv2d<B>,
+    activation21: Relu,
+
+    // conv22: Conv2d<B>,
+    // activation22: Relu,
+
+    // conv23: Conv2d<B>,
+    // activation23: Relu,
+    dropout: Dropout,
+
+    // add
+    conv31: Conv2d<B>,
+    activation31: Relu,
+    // conv32: Conv2d<B>,
+    // activation32: Relu,
+
+    // conv33: Conv2d<B>,
+    // activation33: Relu,
+
+    // conv34: Conv2d<B>,
+    // activation34: Relu,
+
+    // conv35: Conv2d<B>,
+    // reshape 6
+    // transpose
+    // reshape 4
+    // concat
+}
+
+#[derive(Config, Debug)]
+pub struct ModelConfig {
+    #[config(default = "0.5")]
+    dropout: f64,
+}
+
+impl ModelConfig {
+    /// Returns the initialized model.
+    pub fn init<B: Backend>(&self, device: &B::Device) -> Model<B> {
+        Model {
+            conv11: Conv2dConfig::new([16, 3], [3, 3]).init(device),
+            activation11: Relu,
+            // conv12: Conv2dConfig::new([32, 16], [3, 3]).init(device),
+            // activation12: Relu,
+            // conv13: Conv2dConfig::new([64, 32], [3, 3]).init(device),
+            // activation13: Relu,
+            conv21: Conv2dConfig::new([64, 1], [1, 1]).init(device),
+            activation21: Relu,
+            // conv22: Conv2dConfig::new([64, 64], [1, 1]).init(device),
+            // activation22: Relu,
+            // conv23: Conv2dConfig::new([64, 64], [1, 1]).init(device),
+            // activation23: Relu,
+            dropout: DropoutConfig::new(self.dropout).init(),
+            conv31: Conv2dConfig::new([64, 64], [3, 3]).init(device),
+            activation31: Relu,
+            // conv32: Conv2dConfig::new([64, 64], [3, 3]).init(device),
+            // activation32: Relu,
+            // conv33: Conv2dConfig::new([64, 64], [3, 3]).init(device),
+            // activation33: Relu,
+            // conv34: Conv2dConfig::new([64, 64], [3, 3]).init(device),
+            // activation34: Relu,
+            // conv35: Conv2dConfig::new([192, 64], [3, 3]).init(device),
+        }
+    }
+}
+
+impl<B: Backend> Model<B> {
+    /// # Shapes
+    ///   - Images [batch_size, height, width]
+    ///   - Output [batch_size, num_classes]
+    pub fn forward(&self, images: Tensor<B, 3>, inputs: Tensor<B, 3>) -> Tensor<B, 3> {
+        let [batch_size, height, width] = images.dims();
+
+        // Create a channel at the second dimension.
+        let x = images.reshape([batch_size, 1, height, width]);
+
+        let x = self.conv11.forward(x);
+        let x = self.activation11.forward(x);
+
+        let y = inputs.reshape([batch_size, 1, height, width]);
+
+        let y = self.conv21.forward(y);
+        let y = self.dropout.forward(y);
+        let y = self.activation21.forward(y);
+
+        let z = x + y;
+
+        let z = self.conv31.forward(z);
+        let z = self.activation31.forward(z);
+
+        let r = z.reshape([batch_size, height, width]);
+
+        r
+    }
+}
