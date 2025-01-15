c8c375caba72f285d90a180be8175e5aadb65311
9i5BCrUcNX5NmT <machinima40@yandex.ru>
2025-01-16 01:29:49 +0300

    Добавление данных для обучения

--- model/src/main.rs
+++ model/src/main.rs
@@ -34,25 +34,19 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    use burn::data::dataset::InMemDataset;
-    use images::{convert_images_to_image_pixel_data, load_images_from_directory};
+    use images::{convert_images_to_image_pixel_data, load_images_from_directory, process_images};
 
     use super::*;
 
     #[ignore = "for developing"]
     #[test]
-    fn dev_test() {
+    fn generate() {
         type MyBackend = Wgpu<f32, i32>;
 
         let device = burn::backend::wgpu::WgpuDevice::default();
         let artifact_dir = "tmp/test";
 
-        let input_dir = "../data/images/test"; // Путь к входной папке с изображениями
-                                               // let output_dir = "../data/resized_images"; // Путь к выходной папке для сохранения измененных изображений
-                                               // let width = 200;
-                                               // let height = 200;
-
-        // process_images(input_dir, output_dir, width, height).unwrap();
+        let input_dir = "../data/images/test";
 
         let images = load_images_from_directory(input_dir).unwrap();
 
@@ -61,6 +55,26 @@ mod tests {
         // let image_dataset: InMemDataset<images::ImagePixelData> = InMemDataset::new(images);
 
         // crate::inference::infer::<MyBackend>(artifact_dir, device.clone(), images_data[0].clone());
-        crate::inference::infer::<MyBackend>(artifact_dir, device.clone(), images_data[1].clone());
+        crate::inference::infer::<MyBackend>(artifact_dir, device.clone(), images_data[0].clone());
+    }
+
+    #[ignore = "for developing"]
+    #[test]
+    fn resize_images() {
+        let input_dir = "../data/images/raw"; // Путь к входной папке с изображениями
+        let output_dir = "../data/images/resized_images"; // Путь к выходной папке для сохранения измененных изображений
+        let width = 200;
+        let height = 200;
+
+        assert!(process_images(input_dir, output_dir, width, height).is_ok());
+
+        // let images = load_images_from_directory(input_dir).unwrap();
+
+        // let images_data = convert_images_to_image_pixel_data(images);
+
+        // let image_dataset: InMemDataset<images::ImagePixelData> = InMemDataset::new(images);
+
+        // crate::inference::infer::<MyBackend>(artifact_dir, device.clone(), images_data[0].clone());
+        // crate::inference::infer::<MyBackend>(artifact_dir, device.clone(), images_data[1].clone());
     }
 }
