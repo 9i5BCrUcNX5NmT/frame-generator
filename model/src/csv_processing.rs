d1f8e19687913fbdd0d53b07e0253a91f7244137
9i5BCrUcNX5NmT <machinima40@yandex.ru>
2025-01-30 03:54:45 +0300

    Обработка записанных действий

--- model/src/csv_processing.rs
+++ model/src/csv_processing.rs
@@ -1,16 +1,34 @@
 use std::{fs, io};
 
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, Deserialize)]
 struct CsvRecord {
     keys: String,
     mouse: String,
+    buttons: String,
 }
 
-struct KeysRecord {
-    keys: Vec<u8>,   // преобразованные названия клавиш в числа
-    mouse: Vec<f64>, // Движения и скроллинг мыши
+pub struct KeysRecord {
+    keys: Vec<u8>,        // преобразованные названия клавиш в числа
+    mouse: Vec<[i32; 2]>, // Движения и скроллинг мыши
+    buttons: Vec<u8>,
+}
+
+fn xy_to_int(s: &str) -> [i32; 2] {
+    let values: Vec<i32> = s.split(",").map(|s| s.parse::<i32>().unwrap()).collect();
+
+    [values[0], values[1]]
+}
+
+fn key_to_int(key: &str) -> u8 {
+    let v: rdev::Key = serde_json::from_str(key).unwrap();
+    todo!("Превращение key и button в число")
+}
+
+fn button_to_int(key: &str) -> u8 {
+    let v: rdev::Key = serde_json::from_str(key).unwrap();
+    todo!("Превращение key и button в число")
 }
 
 pub fn load_keys_from_directory(dir: &str) -> io::Result<Vec<KeysRecord>> {
@@ -21,9 +39,42 @@ pub fn load_keys_from_directory(dir: &str) -> io::Result<Vec<KeysRecord>> {
         let path = entry.path();
 
         if path.is_file() {
-            let values = todo!("парсинг");
+            let mut reader = csv::Reader::from_path(path).unwrap();
+            for result in reader.deserialize() {
+                let record: CsvRecord = result.unwrap();
 
-            dataset.push(values);
+                // dbg!(&record);
+
+                let keys: Vec<&str> = record.keys.split(", ").collect();
+                let buttons: Vec<&str> = record.buttons.split(", ").collect();
+                let mouse: Vec<&str> = record.mouse.split(", ").collect();
+
+                let keys = if keys.len() > 1 {
+                    keys.iter().map(|&s| key_to_int(s)).collect()
+                } else {
+                    vec![key_to_int(&record.keys)]
+                };
+
+                let mouse = if mouse.len() > 1 {
+                    mouse.iter().map(|&s| xy_to_int(s)).collect()
+                } else {
+                    vec![xy_to_int(&record.mouse)]
+                };
+
+                let buttons = if buttons.len() > 1 {
+                    buttons.iter().map(|&s| button_to_int(s)).collect()
+                } else {
+                    vec![button_to_int(&record.buttons)]
+                };
+
+                todo!();
+
+                dataset.push(KeysRecord {
+                    keys,
+                    mouse,
+                    buttons,
+                });
+            }
         }
     }
 
