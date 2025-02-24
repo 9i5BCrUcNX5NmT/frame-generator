4e24a5159442002367ab378b8556a4b2ac099773
9i5BCrUcNX5NmT <machinima40@yandex.ru>
2025-02-24 19:01:06 +0300

    Улучшение записи датасета

--- recorder/src/main.rs
+++ recorder/src/main.rs
@@ -7,8 +7,8 @@ mod keys_recorder;
 mod video_recorder;
 
 fn main() {
-    std::fs::create_dir_all("../data/keys").unwrap();
-    std::fs::create_dir_all("../data/videos").unwrap();
+    std::fs::create_dir_all("data/keys").unwrap();
+    std::fs::create_dir_all("data/videos").unwrap();
 
     let videos_handle = thread::spawn(|| video_recorder::record());
     let _keys_handle = thread::spawn(|| keys_recorder::record().unwrap());
